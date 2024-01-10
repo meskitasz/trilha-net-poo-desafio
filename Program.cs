f5b3e61f296a1b3f0f4b9f55555c02269eb3de6d
Igor Mesquita <igormesquitab25@gmail.com>
2024-01-09 23:50:31 -0300

    Desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
+Smartphone nokia = new Nokia("8199999999", "111111111111", "Nokia", 128);
+Console.WriteLine("Smartphone Nokia:");
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("\n");
+// Realizado!
+Smartphone iphone = new Iphone("818888888", "2222222222222", "Iphone", 512);
+Console.WriteLine("Smartphone Iphone:");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
